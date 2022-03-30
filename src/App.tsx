f0535753d9482ee21b7185785725c2812f4d15c5
Frederico Velho <fredericovcoelho@gmail.com>
2022-03-29 21:52:15 -0300

    add memo to Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { useEffect, useState } from 'react';
-
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
-
 import { api } from './services/api';
-
+// styles
 import './styles/global.scss';
-
 import './styles/sidebar.scss';
 import './styles/content.scss';
 
@@ -29,9 +26,7 @@ interface MovieProps {
 
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
-
   const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
   const [movies, setMovies] = useState<MovieProps[]>([]);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
 
